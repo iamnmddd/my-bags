348e48ea9575c8004a583c9419d9f069e809a958
iamnmddd <iamnehmad@gmail.com>
2025-06-09 14:51:39 -0600

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'my bags',
+        short_name: 'bags',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#000000',
+        theme_color: '#0f172a',
+        icons: [
+          {
+            src: '/icon-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+});
